4eabe29feb7051e2583322cff734bfbc0f0e1445
Damian Kalinowski <damian.kalinowski@intel.com>
2023-02-15 10:26:30 +0100

    first part

--- demos/gptj_causal_lm/cpp/build_and_run.sh
+++ demos/gptj_causal_lm/cpp/build_and_run.sh
@@ -1,1 +1,2 @@
-g++ main.cpp -L./ -lbingfirtinydll -o main && LD_LIBRARY_PATH=./ ./main
+#g++ main.cpp -L./ -lbingfirtinydll -o main && LD_LIBRARY_PATH=./ ./main
+g++ main.cpp -L./ -l:libblingfiretokdll_static.a -l:libfsaClient.a -o main && LD_LIBRARY_PATH=./ ./main
