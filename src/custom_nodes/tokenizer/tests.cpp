0906fa48346ff555c79519da08169d53d0a2643d
Damian Kalinowski <damian.kalinowski@intel.com>
2023-02-24 13:36:17 +0100

    test fixture

--- src/custom_nodes/tokenizer/tests.cpp
+++ src/custom_nodes/tokenizer/tests.cpp
@@ -106,62 +106,65 @@ void putStringsToTensor(std::vector<std::string> strings, struct CustomNodeTenso
     tensor.name = "texts";
 }
 
-std::vector<int64_t> getTokensFromOutput(struct CustomNodeTensor* outputs, int outputsCount) {
-    struct CustomNodeTensor* textTensor = nullptr;
-    for (int i = 0; i < outputsCount; i++) {
-        if (std::strcmp(outputs[i].name, "tokens") == 0) {
-            textTensor = &(outputs[i]);
+class TokenizerFixtureTest : public ::testing::Test {
+protected:
+    struct output {
+        std::vector<int64_t> tokens;
+        std::vector<int64_t> attention;
+    };
+    void run(std::vector<std::string> in, std::vector<output>& out) {
+        struct CustomNodeTensor inputs[1];
+        struct CustomNodeTensor* outputs = nullptr;
+        int outputsCount = 0;
+        putStringsToTensor(in, inputs[0]);
+        int ret = execute(inputs, 1, &outputs, &outputsCount, params, 2, model);
+        free(inputs[0].data);
+        free(inputs[0].dims);
+        ASSERT_EQ(ret, 0);
+        ASSERT_EQ(outputsCount, 2);
+        std::vector<output> result;
+        result.resize(outputs->dims[0]);
+        for (int i = 0; i < outputsCount; i++) {
+            if (std::strcmp(outputs[i].name, "attention") == 0) {
+                // in: [2, 80]; out: [2, 144]
+                for (int i = 0; i < outputs[i].dims[0]; i++) {
+                    result[i].attention = std::vector<int64_t>(
+                        (int64_t*)outputs[i].data,
+                        (int64_t*)outputs[i].data + (outputs[i].dataBytes / sizeof(int64_t)));
+                }
+            } else if (std::strcmp(outputs[i].name, "tokens") != 0) {
+                for (int i = 0; i < outputs[i].dims[0]; i++) {
+                    result[i].tokens = std::vector<int64_t>(
+                        (int64_t*)outputs[i].data,
+                        (int64_t*)outputs[i].data + (outputs[i].dataBytes / sizeof(int64_t)));
+                }
+            }
         }
+        out = result;
+        ASSERT_EQ(release(outputs, model), 0);
     }
-    if (textTensor == nullptr) {
-        throw std::runtime_error("tokens tensor not found");
+    void SetUp() override {
+        params[0].key = "model_path";
+        params[0].value = "../gpt2.bin";
+        params[1].key = "max_ids_arr_length";
+        params[1].value = "1024";
+        int ret = initialize(&model, params, 2);
+        ASSERT_EQ(ret, 0);
+        ASSERT_NE(model, nullptr);
     }
-    return std::vector<int64_t>((int64_t*)textTensor->data, (int64_t*)textTensor->data + (textTensor->dataBytes / sizeof(int64_t)));
-}
-
-TEST(TokenizerTest, execute) {
-
-/*
-int execute(
-    const struct CustomNodeTensor* inputs,
-    int inputsCount,
-    struct CustomNodeTensor** outputs,
-    int* outputsCount,
-    const struct CustomNodeParam* params,
-    int paramsCount,
-    void* customNodeLibraryInternalManager);
-*/
-    struct CustomNodeTensor inputs[1];
-    struct CustomNodeTensor* outputs = nullptr;
-    int outputsCount = 0;
+    void TearDown() override {
+        int ret = deinitialize(model);
+        ASSERT_EQ(ret, 0);
+    }
     struct CustomNodeParam params[2];
-    params[0].key = "model_path";
-    params[0].value = "../gpt2.bin";
-    params[1].key = "max_ids_arr_length";
-    params[1].value = "1024";
-    Model model(params[0].value);
+    void* model = nullptr;
+};
 
-    std::vector<std::string> texts = {
-        "Эpple pie. How do I renew my virtual smart card?: /Microsoft IT/ 'virtual' smart card certificates for DirectAccess are valid for one year. In order to get to microsoft.com we need to type pi@1.2.1.2.",
-        // "This is a test with a longer text"
-    };
+TEST_F(TokenizerFixtureTest, execute) {
+    std::vector<output> outputs;
+    run({"", "Hello world!", "こんにちは"}, outputs);
+    ASSERT_EQ(outputs.size(), 3);
 
-    std::vector<std::vector<int64_t>> expectedTokens = {
-        {1208,  9397,  2571, 11345,  1012,  2129,  2079, 1045, 20687, 2026,  7484,  6047,
-            4003,  1029,  1024,  1013,  7513,  2009,  1013,  1005,  7484,  1005,  6047,  4003,
-            17987,  2005,  3622,  6305,  9623,  2015,  2024,  9398,  2005,  2028,  2095,  1012,
-            1999,  2344,  2000,  2131,  2000,  7513,  1012,  4012,  2057,  2342,  2000,  2828,
-            14255,  1030,  1015,  1012,  1016,  1012,  1015,  1012,  1016,  1012},
-        // {}
-    };
-
-    putStringsToTensor(texts, inputs[0]);
-    inputs[0].name = "texts";
-    int ret = execute(inputs, 1, &outputs, &outputsCount, params, 2, (void*)&model);
-    ASSERT_EQ(ret, 0);
-    // ASSERT_EQ(release(inputs, (void*)&model), 0);
-    auto tokens = getTokensFromOutput(outputs, outputsCount);
-    ASSERT_EQ(tokens, expectedTokens[0]);
     /*
         // Empty strings
         // Normal texts
