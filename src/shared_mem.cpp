57777428c1fb60d87879dcf556f0dc9ddf6aa09a
Damian Kalinowski <damian.kalinowski@intel.com>
2023-07-13 16:11:47 +0200

    v1

---
+++ src/shared_mem.cpp
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <chrono>
+#include <algorithm>
+
+#include "custom_node_interface.h"
+
+#include <boost/interprocess/allocators/allocator.hpp>
+#include <boost/interprocess/containers/vector.hpp>
+#include <boost/interprocess/managed_shared_memory.hpp>
+
+using namespace boost::interprocess;
+
+// Define an STL compatible allocator of ints that allocates from the managed_shared_memory.
+// This allocator will allow placing containers in the segment
+typedef allocator<uint8_t, managed_shared_memory::segment_manager> ShmemAllocator;
+
+// Alias a vector that uses the previous STL-like allocator so that allocates
+// its values from the segment
+typedef vector<uint8_t, ShmemAllocator> MyVector;
+
+class MyManager {
+    const char* shm_id = "MySharedMemory";
+    bool __cleaned;
+    const size_t max_mem = 1024 * 1024 * 1024;  // 1gb
+    managed_shared_memory segment;
+    const ShmemAllocator alloc_inst;
+public:
+    MyManager() :
+        __cleaned(shared_memory_object::remove(shm_id)),
+        segment(managed_shared_memory(create_only, shm_id, max_mem)),
+        alloc_inst(segment.get_segment_manager())
+    {
+        std::cout << "------------- Hi!" << std::endl;
+    }
+    ~MyManager() {
+        shared_memory_object::remove(shm_id);
+        std::cout << "------------- Bye!" << std::endl;
+    }
+
+    managed_shared_memory& getSegment() { return this->segment; }
+    const ShmemAllocator& getAllocator() const { return this->alloc_inst; }
+};
+
+class measure {
+    std::chrono::steady_clock::time_point now;
+    std::string msg;
+public:
+    measure(const std::string& msg) : msg(msg) {
+        now = std::chrono::steady_clock::now();
+    }
+    ~measure() {
+        auto end = std::chrono::steady_clock::now();
+        std::cout << msg << std::chrono::duration_cast<std::chrono::microseconds>(end - now).count() / 1000.0 << "ms" << std::endl;
+    }
+};
+
+int initialize(void** customNodeLibraryInternalManager, const struct CustomNodeParam* params, int paramsCount) {
+    *customNodeLibraryInternalManager = (void*) new MyManager();
+    return 0;
+}
+
+int deinitialize(void* customNodeLibraryInternalManager){
+    delete (MyManager*)(customNodeLibraryInternalManager);
+    return 0;
+}
+
+int execute(const struct CustomNodeTensor* inputs, int inputsCount, struct CustomNodeTensor** outputs, int* outputsCount, const struct CustomNodeParam* params, int paramsCount, void* customNodeLibraryInternalManager) {
+    auto* mgr = (MyManager*)(customNodeLibraryInternalManager);
+    if (inputsCount != 1) {
+        return 2;
+    }
+
+    if (strcmp(inputs[0].name, "in") != 0) {
+        return 3;
+    }
+
+    const struct CustomNodeTensor* input = &inputs[0];
+
+    if (input->precision != FP32) {
+        return 4;
+    }
+
+    std::string id;
+    MyVector* input_shm;
+
+    {
+        measure m("------- Creating object id time: ");
+        id = std::to_string((int64_t)input->data);
+    } {
+        measure m("------- Constructing vector time: ");
+        input_shm = mgr->getSegment().construct<MyVector>(id.c_str())(mgr->getAllocator());
+    } {
+        measure m("------- Resize vector time: ");
+        input_shm->resize(input->dataBytes);
+    } {
+        measure m("------- Copying tensor to shared memory vector: ");
+        std::memcpy(input_shm->data(), input->data, input->dataBytes);
+    } {
+        measure m("------- Editing shared memory (+1): ");
+        std::for_each(input_shm->begin(), input_shm->end(), [](uint8_t& e) {
+            e += 1;
+        });
+    }
+
+    *outputsCount = 1;
+    *outputs = (struct CustomNodeTensor*)malloc(sizeof(struct CustomNodeTensor) * (*outputsCount));
+    struct CustomNodeTensor* output = (&(*outputs))[0];
+
+    output->name = "out";
+    output->data = (uint8_t*)malloc(input->dataBytes * sizeof(uint8_t));
+    output->dataBytes = input->dataBytes;
+    output->dims = (uint64_t*)malloc(input->dimsCount * sizeof(uint64_t));
+    output->dimsCount = input->dimsCount;
+    memcpy((void*)output->dims, (void*)input->dims, input->dimsCount * sizeof(uint64_t));
+    output->precision = input->precision;
+
+    {
+        measure m("------- Copying from shared memory vector to output tensor: ");
+        std::memcpy(output->data, input_shm->data(), output->dataBytes);
+    } {
+        measure m("------- Removing boost managed vector: ");
+        mgr->getSegment().destroy<MyVector>(id.c_str());
+    }
+
+    return 0;
+}
+
+// Some unit tests are based on a fact that this node library is dynamic and can take shape{1,3} as input.
+int getInputsInfo(struct CustomNodeTensorInfo** info, int* infoCount, const struct CustomNodeParam* params, int paramsCount, void* customNodeLibraryInternalManager) {
+    *infoCount = 1;
+    *info = (struct CustomNodeTensorInfo*) malloc (*infoCount * sizeof(struct CustomNodeTensorInfo));
+    (*info)->name = "in";
+    (*info)->dimsCount = 4;
+    (*info)->dims = (uint64_t*) malloc((*info)->dimsCount * sizeof(uint64_t));
+    (*info)->dims[0] = 10;
+    (*info)->dims[1] = 3;
+    (*info)->dims[2] = 1024;
+    (*info)->dims[3] = 1024;
+    (*info)->precision = FP32;
+    return 0;
+}
+
+int getOutputsInfo(struct CustomNodeTensorInfo** info, int* infoCount, const struct CustomNodeParam* params, int paramsCount, void* customNodeLibraryInternalManager) {
+    *infoCount = 1;
+    *info = (struct CustomNodeTensorInfo*) malloc (*infoCount * sizeof(struct CustomNodeTensorInfo));
+    (*info)->name = "out";
+    (*info)->dimsCount = 4;
+    (*info)->dims = (uint64_t*) malloc((*info)->dimsCount * sizeof(uint64_t));
+    (*info)->dims[0] = 10;
+    (*info)->dims[1] = 3;
+    (*info)->dims[2] = 1024;
+    (*info)->dims[3] = 1024;
+    (*info)->precision = FP32;
+    return 0;
+}
+
+int release(void* ptr, void* customNodeLibraryInternalManager) {
+    free(ptr);
+    return 0;
+}
